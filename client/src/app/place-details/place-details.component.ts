20c75d360f44098f02800e3f3edd31825d2e90ac
luisa86 <lfsh86@gmail.com>
2017-10-03 23:29:54 +0200

    Hecho place-details.componente, y me voy a dormir ya que creo que va a petar mañana y mi compi me mata, jajajja

--- client/src/app/place-details/place-details.component.ts
+++ client/src/app/place-details/place-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import {GuideService} from '../services/guide.service'
+import {ActivatedRoute, Router} from '@angular/router'
 import {Observable} from 'rxjs/Observable'
 
 @Component({
@@ -9,11 +10,32 @@ import {Observable} from 'rxjs/Observable'
 })
 export class PlaceDetailsComponent implements OnInit {
 
-  guides;
-  constructor(public guideService:GuideService) { }
+  guide;
+  constructor(
+    private router:Router,
+    private route:ActivatedRoute,
+    private guideService: GuideService) {}
+
 
   ngOnInit() {
-    this.guideService.getList().subscribe(result => this.guides = result)
+    this.route.params.subscribe(params => {
+          this.getGuideDetails(params['id'])
+        });  }
+
+
+        getGuideDetails(id) {
+    this.guideService.get(id)
+      .subscribe((guide) => {
+        this.guide = guide
+      });
+  }
+  deleteGuide() {
+    if (window.confirm('Are you sure?')) {
+      this.guideService.remove(this.guide._id)
+        .subscribe(() => {
+          this.router.navigate([''])
+        });
+    }
   }
 
 }
