fcaf8e137175386dfbcfe29cae4f915385158e27
luisa86 <lfsh86@gmail.com>
2017-10-03 22:07:24 +0200

    añadido campo de foto para user

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,76 +1,79 @@
-const express = require('express');
-const passport = require('passport');
-const path = require('path');
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
-const debug = require('debug')("angularauth:"+path.basename(__filename).split('.')[0]);
+const express = require('express')
+const passport = require('passport')
+const multer = require('multer')
+const image = multer({ dest: './public/pictures/' })
+const path = require('path')
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const debug = require('debug')("angularauth:"+path.basename(__filename).split('.')[0])
 
-var authRoutes = express.Router();
+var authRoutes = express.Router()
 
 /* GET home page. */
 authRoutes.post('/signup', (req, res, next) => {
-  const {username, password} = req.body;
+  const {username, password, image} = req.body
 
   if (!username || !password)
-    return res.status(400).json({ message: 'Provide username and password' });
+    return res.status(400).json({ message: 'Provide username and password' })
 
-  debug('Find user in DB');
+  debug('Find user in DB')
 
   User.findOne({ username },'_id').exec().then(user =>{
     if(user)
-      return res.status(400).json({ message: 'The username already exists' });
+      return res.status(400).json({ message: 'The username already exists' })
 
-    const salt     = bcrypt.genSaltSync(10);
-    const hashPass = bcrypt.hashSync(password, salt);
-    debug('creating user');
+    const salt     = bcrypt.genSaltSync(10)
+    const hashPass = bcrypt.hashSync(password, salt)
+    debug('creating user')
     const theUser = new User({
       username,
-      password: hashPass
-    });
+      password: hashPass,
+      image
+    })
     return theUser.save()
     .then(user =>{
       req.login(user, (err) => {
         if (err)
-          return res.status(500).json({ message: 'Something went wrong' });
+          return res.status(500).json({ message: 'Something went wrong' })
 
-        res.status(200).json(req.user);
-      });
+        res.status(200).json(req.user)
+      })
     })
   })
   .catch(e => {
-    console.log(e);
+    console.log(e)
     res.status(400).json({ message: 'Something went wrong' })
-  });
-});
+  })
+})
 
 authRoutes.post('/login', (req, res, next) => {
   passport.authenticate('local', (err, user, failureDetails) => {
     if (err)
-      return res.status(500).json({ message: 'Something went wrong' });
+      return res.status(500).json({ message: 'Something went wrong' })
 
     if (!user)
-      return res.status(401).json(failureDetails);
+      return res.status(401).json(failureDetails)
 
     req.login(user, (err) => {
       if (err)
-        return res.status(500).json({ message: 'Something went wrong' });
+        return res.status(500).json({ message: 'Something went wrong' })
 
       // We are now logged in (notice req.user)
-      res.status(200).json(req.user);
-    });
-  })(req, res, next);
-});
+      res.status(200).json(req.user)
+    })
+  })(req, res, next)
+})
 
 authRoutes.get('/logout', (req, res, next) => {
-  req.logout();
-  res.status(200).json({ message: 'Success' });
-});
+  req.logout()
+  res.status(200).json({ message: 'Success' })
+})
 
 authRoutes.get('/loggedin', (req, res, next) => {
   if (req.isAuthenticated())
-    return res.status(200).json(req.user);
-  res.status(403).json({ message: 'Unauthorized' });
-});
+    return res.status(200).json(req.user)
+  res.status(403).json({ message: 'Unauthorized' })
+})
 
 
-module.exports = authRoutes;
+module.exports = authRoutes
