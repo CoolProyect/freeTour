425d4830eacef68bdda2de9a5acdd4f82e1716a8
luisa86 <lfsh86@gmail.com>
2017-10-06 16:45:52 +0200

    vuelve el perro arrepentido con el CORS entre las piernas

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -6,24 +6,24 @@ const checkIDParam = require('../middlewares/checkIfExists')
 
 
 router.get('/guides', guideController.list)
-router.get('/guides/:id', checkIDParam, guideController.show)
+router.get('/guides/', checkIDParam, guideController.show)
 router.post('/guides/new', guideController.create)
-router.put('/guides/:id/edit', checkIDParam, guideController.update)
-router.get('/guides/:id/delete', checkIDParam,guideController.remove)
+router.put('/guides//edit', checkIDParam, guideController.update)
+router.get('/guides//delete', checkIDParam,guideController.remove)
 
 router.get('/city', cityController.list)
-router.get('/city/:id',checkIDParam, cityController.show)
+router.get('/city/',checkIDParam, cityController.show)
 router.post('/city/new', cityController.create)
-router.put('/city/:id/edit',checkIDParam, cityController.update)
-router.delete('/city/:id/delete', checkIDParam,cityController.remove)
+router.put('/city//edit',checkIDParam, cityController.update)
+router.delete('/city//delete', checkIDParam,cityController.remove)
 
 router.get('/point-interest', pointOfInterestController.list)
 router.get('/point-interest/gmaps', pointOfInterestController.point)
 router.post('/point-interest/new', pointOfInterestController.create)
 
-router.get('/point-interest/:id',checkIDParam, pointOfInterestController.show)
-router.put('/point-interest/:id/edit',checkIDParam, pointOfInterestController.update)
-router.delete('/point-interest/:id/delete', checkIDParam, pointOfInterestController.remove)
+router.get('/point-interest/',checkIDParam, pointOfInterestController.show)
+router.put('/point-interest//edit',checkIDParam, pointOfInterestController.update)
+router.delete('/point-interest//delete', checkIDParam, pointOfInterestController.remove)
 
 
 module.exports = router
