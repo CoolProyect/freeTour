620fd628c05be3b7b5e781401ec3ce93d556cc8c
luisa86 <lfsh86@gmail.com>
2017-10-06 17:42:24 +0200

    PAPU nuestro nuevo heroe, después de Marc version 2.0

--- client/src/app/services/buscador.service.ts
+++ client/src/app/services/buscador.service.ts
@@ -8,13 +8,14 @@ import { environment } from '../../environments/environment'
 
 @Injectable()
 export class BuscadorService {
+  private options = { withCredentials: true }
 
   BASE_URL: string = 'http://localhost:3000'
 
   constructor(private http: Http) { }
 
   getPoint(city) {
-    return this.http.get(`${this.BASE_URL}/point-interest/gmaps/?city=${city}`, city)
+    return this.http.get(`${this.BASE_URL}/point-interest/gmaps/?city=${city}`, this.options)
       .map((res) => res.json());
   }
 
